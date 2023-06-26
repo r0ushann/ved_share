19dea619627b9cc953193def65fe48fce45cc18c
r0ushann <roushan007kr@gmail.com>
2023-06-26 16:21:27 +0530

    commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -21,13 +21,13 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
-        {/**<Image
-          src='/assets/images/logo.svg'
+        <Image
+           src='/images/download.png'
           alt='logo'
-          width={30}
+           width={30}
           height={30}
-          className='object-contain'
-        /> */}
+           className="rounded-full"
+      /> 
         <h1 className='orange_gradient text-3xl font-bold'>Vedanta</h1>
       </Link>
 
